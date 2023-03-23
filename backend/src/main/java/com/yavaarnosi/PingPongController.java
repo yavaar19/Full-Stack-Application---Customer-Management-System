21a4e280944e0272c1938ca47308aa6bcbd0ed9d
Yavaar Nosimohomed <ynosimo@wgu.edu>
2023-03-23 03:18:38 -0400

    CD Workflow with new controller

---
+++ backend/src/main/java/com/yavaarnosi/PingPongController.java
@@ -0,0 +1,18 @@
+package com.yavaarnosi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingPongController {
+
+    record PingPong(String result){}
+
+    @GetMapping("/ping")
+    public PingPong getPingPong(){
+
+        return new PingPong("Pong");
+
+    }
+
+}
