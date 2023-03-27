9054a8377df833e24059902fa38627ebaf3bdd13
Yavaar Nosimohomed <ynosimo@wgu.edu>
2023-03-27 02:12:53 -0400

    Added front end components

---
+++ frontend/react/src/App.jsx
@@ -0,0 +1,91 @@
+import {Wrap, WrapItem, Spinner, Text} from '@chakra-ui/react';
+import SidebarWithHeader from "./components/shared/SideBar.jsx";
+import { useEffect, useState } from "react";
+import {getCustomers} from "./services/client.js";
+import CardWithImage from "./components/card.jsx";
+
+
+const App = () => {
+
+    const [customers, setCustomers] = useState([])
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+
+        setLoading(true);
+
+        getCustomers().then(res => {
+
+            setCustomers(res.data)
+            console.log(res)
+
+        }).catch(err => {
+
+            console.log(err)
+
+        }).finally(() => {
+
+            setLoading(false);
+
+        })
+
+    }, [])
+
+    if (loading) {
+
+        return (
+
+            <SidebarWithHeader>
+
+                <Spinner
+                    thickness='4px'
+                    speed='0.65s'
+                    emptyColor='gray.200'
+                    color='blue.500'
+                    size='xl'
+                />
+
+        </SidebarWithHeader>
+
+        )
+
+    }
+
+    if(customers.length <=0 ){
+
+        return (
+
+            <SidebarWithHeader>
+
+                <Text>No customers available</Text>
+
+            </SidebarWithHeader>
+
+        )
+
+    }
+
+    return(
+
+        <SidebarWithHeader>
+
+            <Wrap justify ={"center"} spacing={"30px"}>
+
+            {customers.map((customer, index) =>
+
+                <WrapItem key={index}>
+
+                    <CardWithImage {...customer} />
+
+                </WrapItem>
+
+            )}
+
+        </Wrap>
+
+        </SidebarWithHeader>
+
+    )
+
+}
+export default App;
