ec75b0b8bd75295be17b3687ab9e5bf584aa1e73
Yavaar Nosimohomed <ynosimo@wgu.edu>
2023-04-04 06:26:01 -0400

    Frontend Deployment

--- backend/src/main/java/com/yavaarnosi/config/WebMvcConfig.java
+++ backend/src/main/java/com/yavaarnosi/config/WebMvcConfig.java
@@ -1,11 +1,7 @@
 package com.yavaarnosi.config;
 
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -21,24 +17,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Value("#{'${cors.allowed-methods}'.split(',')}")
     private List<String> allowedMethods;
 
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//
-//        CorsRegistration corsRegistration = registry.addMapping("api/**");
-//
-//        allowedOrigins.forEach(corsRegistration::allowedOrigins);
-//        allowedMethods.forEach(corsRegistration::allowedMethods);
-//
-//    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
 
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/api/**", config);
-        return new CorsFilter();
+        CorsRegistration corsRegistration = registry.addMapping("api/**");
+
+        allowedOrigins.forEach(corsRegistration::allowedOrigins);
+        allowedMethods.forEach(corsRegistration::allowedMethods);
+
     }
+
 }
--- backend/src/main/java/com/yavaarnosi/customer/CustomerController.java
+++ backend/src/main/java/com/yavaarnosi/customer/CustomerController.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/customers")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:8080")
 public class CustomerController {
 
     private final CustomerService customerService;
