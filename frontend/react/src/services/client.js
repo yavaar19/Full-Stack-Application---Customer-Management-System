ad0f99aa8e8d9430e6e31519092bc7289eed1c73
Yavaar Nosimohomed <ynosimo@wgu.edu>
2023-04-04 07:07:42 -0400

    Frontend Deployment

--- frontend/react/src/services/client.js
+++ frontend/react/src/services/client.js
@@ -4,7 +4,13 @@ export const getCustomers = async () =>{
 
     try {
 
-        return await axios.get(`${ import.meta.env.VITE_API_BASE_URL }/api/v1/customers`)
+        return await axios.get(`${ import.meta.env.VITE_API_BASE_URL }/api/v1/customers`, {
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+            }
+        })
 
     }catch (e){
 
@@ -19,7 +25,13 @@ export const saveCustomer = async (customer) => {
     try {
 
         return await axios.post(`${ import.meta.env.VITE_API_BASE_URL }/api/v1/customers`,
-            customer
+            customer, {
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                }
+            }
             )
 
     } catch (e){
